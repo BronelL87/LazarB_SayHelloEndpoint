57de6504dfd2440405ef15c611091f47b10dcce9
BronelL87 <bronellazar@gmail.com>
2024-01-08 16:00:47 -0800

    first commit

---
+++ Controllers/SayHello.cs
@@ -0,0 +1,36 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ObjectPool;
+
+namespace LazarB_SayHelloEndpoint.Controllers;
+[ApiController]
+[Route("controller")]
+
+public class SayHello : ControllerBase{
+    public List<string> GreetUser = new();
+
+public SayHello()
+    {
+     
+    }
+
+    [HttpGet]
+    [Route("GreetUser")]
+    public List<string> GetName(){
+        return GreetUser;
+    } 
+
+
+    [HttpPost]
+    [Route("AddName/{yourName}")]
+
+    public List<string> AddName(string yourName){
+        GreetUser.Add("Hello " + yourName);
+        return GreetUser;
+    }
+
+
+}
+    
+
